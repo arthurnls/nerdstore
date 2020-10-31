cbb80583e16ed7602c7cac0b3c0fcd25203f531b
Arthur Saraiva <arthurnl@gmail.com>
2020-10-30 19:16:54 -0500

    fixed bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,20 @@
 Rails.application.routes.draw do
-  get 'products/index'
-  get 'products/show'
-  get 'categories/index'
-  get 'categories/show'
-  get 'departments/show'
-  get 'pages/account'
-  get 'pages/about'
-  get 'pages/contact'
-  get 'pages/cart'
-  get 'pages/checkout'
+  root to: "products#index"
+
+  get "pages/account"
+  get "pages/about"
+  get "pages/contact"
+  get "pages/cart"
+  get "pages/checkout"
+
+  get "products/index"
+  get "products/show"
+
+  get "categories/index"
+  get "categories/show"
+
+  get "departments/show"
+
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
