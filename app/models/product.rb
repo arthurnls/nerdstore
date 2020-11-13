0e28af13606f1eba55221d55f09b0488e34d9167
Arthur Saraiva <arthurnl@gmail.com>
2020-11-13 14:32:27 -0600

    small styles fix

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,13 +1,8 @@
 class Product < ApplicationRecord
   belongs_to :brand
   belongs_to :category
-  # has_many :images
-
-  ###################
-  # Adding image upload
   has_many_attached :images
-  ###################
-
+  
   # Many to Many Relationship to Users table
   has_many :order_details
   has_many :orders, through: :order_details
