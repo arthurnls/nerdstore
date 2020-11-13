9ba6bc7dfa3e976f94973a0a6c388d33dd7dfd36
Arthur Saraiva <arthurnl@gmail.com>
2020-11-13 16:38:52 -0600

    tweaked how images are shown

--- app/admin/products.rb
+++ app/admin/products.rb
@@ -1,15 +1,31 @@
 ActiveAdmin.register Product do
-  permit_params :name, :description, :price, :stock_quantity, :brand_id, :category_id, :images
+  permit_params :name, :description, :price, :stock_quantity, :brand_id, :category_id, images: []
 
   # Formtastic gem
-  form do |f|
+  form(html: { multipart: true }) do |f|
     f.semantic_errors # show errors on :base
     f.inputs          # builds an input field for every attribute
     f.inputs do
-      f.input :images, as: :file
+      f.input :images, as: :file, input_html: { multiple: true }
     end
     f.actions         # adds the Submit and Cancel buttons
   end
+
+  show do
+    attributes_table do
+      row :images do
+        div do
+          product.images.each do |img|
+            div do |d|
+              image_tag url_for(img), size: "150x150"
+            end
+          end
+        end
+      end
+    end
+    default_main_content
+  end
+
   #
   # or
   #
