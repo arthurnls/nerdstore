e13118776856f8bbd2e104f3ce3a1f0abf68881d
Arthur Saraiva <arthurnl@gmail.com>
2020-11-18 21:47:33 -0600

    finished shopping cart implementation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :initialize_session
   before_action :load_cart
-  #   before_action :increment_visit_count
 
   private
 
@@ -11,10 +10,5 @@ class ApplicationController < ActionController::Base
 
   def load_cart
     @cart = Product.find(session[:cart])
-  end
-
-  def increment_visit_count
-    # session[:visit_count] += 1
-    # @visit_count = session[:visit_count]
   end
 end
--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -3,7 +3,14 @@ class OrdersController < ApplicationController
 
   def show; end
 
-  def cart; end
+  def cart
+    @subtotal = 0
+    @total_items = 0
+    @cart.each do |product|
+      @subtotal += product.price
+      @total_items += 1
+    end
+  end
 
   def add_to_cart
     product_id = params[:id].to_i
@@ -11,5 +18,9 @@ class OrdersController < ApplicationController
     redirect_to request.referer
   end
 
-  def remove_from_cart; end
+  def remove_from_cart
+    product_id = params[:id].to_i
+    session[:cart].delete(product_id)
+    redirect_to request.referer
+  end
 end
